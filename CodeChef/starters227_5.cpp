d7fa1c8164153dc49764a61c9bff1821dab8ff3a
Abhishek Kumar Yadav <abhy9393@gmail.com>
2026-07-08 11:53:04 +0530

    Update starters227_5.cpp

--- CodeChef/starters227_5.cpp
+++ CodeChef/starters227_5.cpp
@@ -1,3 +1,21 @@
+/*
+Problem : Equal Elements
+Platform : CodeChef
+Difficulty : 1738
+
+Approach :
+- Greedy + Map
+- Traverse from right to left.
+
+Observation :
+- Left se future decisions lene padte hain.
+- Right se last pair ko fix kar sakte hain.
+- Pair milte hi mp.clear().
+
+Time : O(N log N)
+Space : O(N)
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
